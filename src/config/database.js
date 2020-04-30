98c478e56f7155d2371f56e61fab0909d0a09075
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2020-04-29 21:04:06 -0300

    Cadastro de usuario

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 module.exports = {
-    dialects: 'postgres',
+    dialect: 'postgres',
     host: 'localhost',
+    port: 5433,
     username: 'postgres',
     password: 'docker',
     database: 'gobarber',
