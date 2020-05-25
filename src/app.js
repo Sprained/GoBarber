0ab27fee9b8fb6c4d274375a36e04b13906f2b2d
Gabriel Resende <gabriel.almeida.resende@gmail.com>
2020-05-25 15:38:16 -0300

    monitoramente de erros

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import 'express-async-errors';
 import path from 'path';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
+
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -8,17 +12,21 @@ class App {
     constructor(){
         this.server = express();
 
+        Sentry.init(sentryConfig);
+
         this.middlewares();
         this.routes();
     }
 
     middlewares(){
+        this.server.use(Sentry.Handlers.requestHandler());
         this.server.use(express.json());
         this.server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
     }
 
     routes(){
-        this.server.use(routes)
+        this.server.use(routes);
+        this.server.use(Sentry.Handlers.errorHandler());
     }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://ca6c6a30c3a9423c8749cfe263442229@o397956.ingest.sentry.io/5253010',
+}
